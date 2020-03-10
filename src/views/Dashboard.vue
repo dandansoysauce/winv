aa0079324b2748631afe8a3cc625936c95240adf
Dani Ona <ona.dani.a@gmail.com>
2020-03-11 04:08:46 +0800

    add header

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,20 @@
 <template>
-  <div></div>
+  <div class="container is-fluid">
+    <Header></Header>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Header from '@/components/Header.vue';
 
-@Component
+@Component({
+  components: {
+    Header,
+  },
+})
 export default class Dashboard extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+</style>
