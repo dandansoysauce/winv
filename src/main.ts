313c93d44dfe48d1fd336dcdfbc955f13b76dcc4
Dani Ona <ona.dani.a@gmail.com>
2020-03-26 23:45:52 +0800

    add firebase and firestore support

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import VueMaterial from 'vue-material';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import firebase from 'firebase/app'
+import { firestorePlugin } from 'vuefire'
+import 'firebase/firestore'
+
+export const db = firebase.initializeApp({ projectId: 'simplewonder-inventory' }).firestore()
 
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default-dark.css';
@@ -10,6 +15,7 @@ import 'vue-material/dist/theme/default-dark.css';
 Vue.config.productionTip = false;
 
 Vue.use(VueMaterial);
+Vue.use(firestorePlugin);
 
 new Vue({
   router,
