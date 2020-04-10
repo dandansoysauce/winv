0c74e43a06ac0ca2cfe1b97cdc8627e600da6b81
Dani Ona <ona.dani.a@gmail.com>
2020-04-11 03:43:10 +0800

    fix dashboard container

--- src/views/DashboardHome.vue
+++ src/views/DashboardHome.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <h1>Dashboard</h1>
-  </div>
+  <v-container class="fill-height align-start">
+    <div class="d-flex flex-column fill-height width-100">
+      <h1>Dashboard</h1>
+    </div>
+  </v-container>
 </template>
 
 <script lang="ts">
